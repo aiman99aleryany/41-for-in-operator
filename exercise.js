1f0981b7fa9cbf762246ea9a7047b7ca919f7697
aiman99aleryany <102668388+aiman99aleryany@users.noreply.github.com>
2022-09-19 15:45:04 +0300

    init commit

---
+++ exercise.js
@@ -0,0 +1,15 @@
+// Print the key of the car object.
+
+// let car = {
+//   name: "Fiat",
+//   color: "red",
+// };
+
+let car = {
+  name: "Fiat",
+  color: "red",
+};
+
+for (let car_key in car) {
+  console.log(car_key);
+}
